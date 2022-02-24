caf06d9b0604ff312da3b22ac5bc26e3a15d8099
hyeon <liebe244@naver.com>
2022-02-24 22:19:41 +0900

    styled components

--- src/App.js
+++ src/App.js
@@ -1,5 +1,28 @@
+import styled from "styled-components";
+
+const Father = styled.div`
+  display: flex;
+`;
+
+const Box = styled.div`
+  background-color: ${(props) => props.bgColor};
+  width: 100px;
+  height: 100px;
+`;
+
+const Text = styled.span`
+  color: white;
+`;
+
 function App() {
-  return null;
+  return (
+    <Father>
+      <Box bgColor="tomato">
+        <Text>Hello</Text>
+      </Box>
+      <Box bgColor="teal" />
+    </Father>
+  );
 }
 
 export default App;
