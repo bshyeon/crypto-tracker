ceecb35cd8fe5f8993ad1ea8d1d13e346c0f382a
hyeon <liebe244@naver.com>
2022-03-06 23:52:47 +0900

    coins home

--- src/theme.ts
+++ src/theme.ts
@@ -1,7 +1,7 @@
 import { DefaultTheme } from "styled-components";
 
 export const theme: DefaultTheme = {
-  bgColor: "black",
-  textColor: "white",
-  accentColor: "red",
+  bgColor: "#192a56",
+  textColor: "#44bd32",
+  accentColor: "#7f8fa6",
 };
