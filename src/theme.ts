1f63f5e7e2a299c626b4c8200ea99d926944e5fc
hyeon <liebe244@naver.com>
2022-03-07 22:30:29 +0900

    change color

--- src/theme.ts
+++ src/theme.ts
@@ -2,6 +2,6 @@ import { DefaultTheme } from "styled-components";
 
 export const theme: DefaultTheme = {
   bgColor: "#192a56",
-  textColor: "#44bd32",
-  accentColor: "#7f8fa6",
+  textColor: "white",
+  accentColor: "#8c7ae6",
 };
